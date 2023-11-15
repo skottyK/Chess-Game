f71410c0cb3a86d7e2f0a9c556db4442c8a2f514
skottyK <skotty@wisc.edu>
2023-11-14 19:34:39 -0600

    hope this works

---
+++ LeaderBoard.vue
@@ -0,0 +1,77 @@
+<template>
+  <!-- This is the leaderboard section - considerable work still to be done -->
+    <div class="LeaderBoard">
+      <!-- Title of the leaderboard -->
+      <h1>Chess Games Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <!-- This is the components of the leaderboard -->
+            <th>Rank</th>
+            <th>Player</th>
+            <th>Wins</th>
+            <th>Losses</th>
+            <th>Draws</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- This is the data that will be displayed in the leaderboard -->
+          <!-- Loop through to display each data for evrey player -->
+          <tr v-for="(entry, index) in leaderboardData" :key="entry.player">
+            <td>{{ index + 1 }}</td>
+            <td>{{ entry.player }}</td>
+            <td>{{ entry.wins }}</td>
+            <td>{{ entry.losses }}</td>
+            <td>{{ entry.draws }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</template>
+
+<script>
+/*
+  * The LeaderBoard component is the leaderboard page of the application,
+  * containing options to view the leaderboard and its corresponding data for each player.
+  * @component
+*/
+export default {
+  name: 'LeaderBoard',
+  data () {
+    return {
+      // This data would typically come from an API call
+      // TODO: implement this from database
+      leaderboardData: [
+        { player: 'Player1', wins: 20, losses: 5, draws: 3 },
+        { player: 'Player2', wins: 18, losses: 7, draws: 1 }
+        // ... more players
+      ]
+    }
+  }
+}
+</script>
+  <style scoped>
+  /* Styles for the leaderboard page */
+  .LeaderBoard {
+    max-width: 600px;
+    margin: 2rem auto; /* Increased top margin */
+    padding: 1rem;
+  }
+  /* Styles for the leaderboard table */
+  table {
+    width: 100%;
+    border-collapse: collapse;
+  }
+  /* Styles for the leaderboard table header */
+  th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+  }
+  th {
+    background-color: #f4f4f4;
+  }
+  tr:hover {
+    background-color: #f5f5f5;
+  }
+</style>
---
+++ MyAccount.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- This is the My account component that displays your account info and game data -->
+    <div class="my-account">
+      <!-- Title of page -->
+      <h1>My Account</h1>
+      <div class="account-details">
+        <!-- Account details go here. Replace with actual data bindings. -->
+        <p><strong>Username:</strong> YourUsername</p>
+        <p><strong>Email:</strong> your.email@example.com</p>
+        <!-- Add more account details as needed -->
+      </div>
+      <div class="account-actions">
+        <!-- Add actions like editing account, changing password, etc. -->
+        <button @click="goToLeaderboard">View Leaderboard</button>
+      </div>
+    </div>
+</template>
+<script>
+/*
+  * The MyAccount component is the my account page of the application,
+  * containing options to view your account details and game data.
+  * @component
+*/
+export default {
+  name: 'MyAccount',
+  methods: {
+    goToLeaderboard () {
+      this.$router.push('/LeaderBoard')
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* CSS Styles for the my account page */
+  .my-account {
+    max-width: 600px;
+    margin: auto;
+    padding: 20px;
+  }
+  /* Styles for the account details section */
+  .account-details {
+    margin-bottom: 20px;
+  }
+  /* style for the buttons */
+  button {
+    padding: 10px 15px;
+    background-color: #3498db;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: #2980b9;
+  }
+  </style>
